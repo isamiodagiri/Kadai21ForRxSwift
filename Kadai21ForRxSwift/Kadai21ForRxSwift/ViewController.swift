64b8135d3e59979fe3237db30529acca98579a28
Isami Odagiri <gurentosouen@gmail.com>
2020-09-07 15:03:26 +0900

    初期プロジェクト設置

---
+++ Kadai21ForRxSwift/Kadai21ForRxSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kadai21ForRxSwift
+//
+//  Created by Isami Odagiri on 2020/09/07.
+//  Copyright © 2020 Isami Odagiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
